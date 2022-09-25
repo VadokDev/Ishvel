ab228d1442e20b9294a485e2e7b5e0e8b560899e
VadokDev <gonzalo.fdez06@gmail.com>
2022-09-25 03:33:32 -0300

    added pyodide

---
+++ editor/src/components/molecules/InputArea.js
@@ -0,0 +1,8 @@
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+
+const InputArea = () => {
+  return <TextareaAutosize rows={405} />;
+};
+
+export default InputArea;
