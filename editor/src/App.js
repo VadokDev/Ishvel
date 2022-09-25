ee1e44d542b094f48ed534086cf215538ef2272a
VadokDev <gonzalo.fdez06@gmail.com>
2022-09-25 03:33:32 -0300

    Revert "added pyodide"
    This reverts commit ab228d1442e20b9294a485e2e7b5e0e8b560899e.

--- editor/src/App.js
+++
@@ -1,7 +0,0 @@
-import Editor from "./pages/Editor";
-
-function App() {
-  return <Editor></Editor>;
-}
-
-export default App;
--- editor/src/components/molecules/InputArea.js
+++
@@ -1,8 +0,0 @@
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
-
-const InputArea = () => {
-  return <TextareaAutosize rows={405} />;
-};
-
-export default InputArea;
